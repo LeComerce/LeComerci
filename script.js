c8ecbeb074fa7987a229e7e3daf2b249fc25cabb
LeComerce <nk129867@gmail.com>
2025-03-06 00:23:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents form from actually submitting
+    alert("Thank you for contacting us!");
+});
